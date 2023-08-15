ceba19764e13234dfc7f5712eb7e090316228bf6
MD Tarekul Islam Sabbir <tisabbir0@gmail.com>
2023-08-15 22:26:11 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+//Square
+var base = prompt("Enter base of the square = ");
+var height = prompt("Enter height of the square = ");
+
+area = base * height;
+
+document.write("Area of the Square= " + area + "<br/>");
+
+//Triangle
+var base = prompt("Enter base of the triangle = ");
+var height = prompt("Enter height of the triangle = ");
+
+area = base * height;
+
+document.write("Area of the Triangle= " + area + "<br/>");
